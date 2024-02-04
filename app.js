eaeb2a7cbeffe5fcf2c9f18280a0b863b7591711
Ali Zain <alizain10166@gmail.com>
2024-02-04 20:17:36 +0100

    Fix bug for winner output

--- app.js
+++ app.js
@@ -4,42 +4,39 @@ let resetbtn = document.querySelector("reset");
 let turnO = true;
 
 const winPatterns = [
-    [1,2,3],
-    [4,5,6],
-    [7,8,9],
-    [1,4,7],
-    [2,5,8],
-    [3,6,9],
-    [1,5,9],
-    [3,5,7]
-]
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [1, 4, 7],
+  [2, 5, 8],
+  [3, 6, 9],
+  [1, 5, 9],
+  [3, 5, 7],
+];
 
 boxes.forEach((box) => {
-    box.addEventListener("click", () => {
-        if(turnO){
-            box.innerText = "O";
-            turnO = false;
-        }else{
-            box.innerText = "X";
-            turnO = true;
-        }
-        box.disabled = true;
-
-        checkWinner();
-    })
-
-})
+  box.addEventListener("click", () => {
+    if (turnO) {
+      box.innerText = "O";
+    } else {
+      box.innerText = "X";
+    }
+    box.disabled = true;
+    turnO = !turnO;
+    checkWinner();
+  });
+});
 
 const checkWinner = () => {
-  for(pattern of winPatterns){
-    let pos1Val = boxes[pattern[0]].innerText
-    let pos2Val = boxes[pattern[1]].innerText
-    let pos3Val = boxes[pattern[2]].innerText
-   
-    if(pos1Val != "" && pos2Val != "" && pos3Val != ""){
-        if(pos1Val === pos2Val && pos2Val === pos3Val){
-            console.log("winner");
-        }
+  for (pattern of winPatterns) {
+    let pos1Val = boxes[pattern[0] - 1].innerText;
+    let pos2Val = boxes[pattern[1] - 1].innerText;
+    let pos3Val = boxes[pattern[2] - 1].innerText;
+
+    if (pos1Val != "" && pos2Val != "" && pos3Val != "") {
+      if (pos1Val === pos2Val && pos2Val === pos3Val) {
+        console.log("winner");
+      }
     }
   }
-}
+};
